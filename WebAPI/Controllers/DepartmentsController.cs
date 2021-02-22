12a99db8413b1a6ce07549bcb6eb18727017c5aa
LatyshevViktor <latyshev.vicktor@yandex.ru>
2021-02-22 22:42:24 +0300

    22.02.2021 Создание backend части для приложения "Система управления предприятием"
    Signed-off-by: LatyshevViktor <latyshev.vicktor@yandex.ru>

---
+++ WebAPI/Controllers/DepartmentsController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Data;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentsController : ControllerBase
+    {
+        private readonly WebAPIContext _dbContext;
+
+        public DepartmentsController(WebAPIContext context)
+        {
+            _dbContext = context;
+        }
+
+        /// <summary>
+        /// Редактируем отдел
+        /// </summary>
+        /// <param name="department"></param>
+        /// <returns></returns>
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditDepartment(Departments department)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Update(department);
+                await _dbContext.SaveChangesAsync();
+                return Ok(department);
+            }
+            return BadRequest();
+        }
+
+        /// <summary>
+        /// Добавляем отдел
+        /// </summary>
+        /// <param name="department"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<ActionResult<Departments>> AddDepartment(Departments department)
+        {
+            _dbContext.Departments.Add(department);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(department);
+        }
+
+        /// <summary>
+        /// Удаляем отдел
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Departments>> DeleteDepartment(int id)
+        {
+            var department = await _dbContext.Departments.FindAsync(id);
+            if (department == null)
+                return BadRequest();
+
+            _dbContext.Departments.Remove(department);
+            await _dbContext.SaveChangesAsync();
+
+            return department;
+        }
+    }
+}
---
+++ WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Data;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly WebAPIContext _dbContext;
+
+        public UsersController(WebAPIContext context)
+        {
+            _dbContext = context;
+        }
+
+        /// <summary>
+        /// Список всех пользователей
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IQueryable<Users> GetAllUser()
+        {
+            return _dbContext.Users.Include(b => b.Dep);
+        }
+
+        /// <summary>
+        /// Получаем конкретного пользователя
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Users>> GetUser(int id)
+        {
+            var user = await _dbContext.Users.FindAsync(id);
+
+            if (user == null)
+                return BadRequest();
+
+            return user;
+        }
+
+        /// <summary>
+        /// Изменяем пользователя по Id
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditUser(Users user)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Users.Update(user);
+                await _dbContext.SaveChangesAsync();
+                return Ok(user);
+            }
+
+            return BadRequest();
+        }
+
+        
+        [HttpPost]
+        public async Task<ActionResult<Users>> AddUser(Users user)
+        {
+            _dbContext.Users.Add(user);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+        
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Users>> DeleteUser(int id)
+        {
+            var user = await _dbContext.Users.FindAsync(id);
+            if (user == null)
+                return BadRequest();
+
+            _dbContext.Users.Remove(user);
+            await _dbContext.SaveChangesAsync();
+
+            return user;
+        }
+    }
+}
